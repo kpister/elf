c7ed73e6aa39593c0d87a54d1afa14eb19c99e4b
Kaiser Pister <pisterk@gmail.com>
2022-05-18 11:04:42 -0500

    black

--- elf.py
+++ elf.py
@@ -12,6 +12,7 @@ import yaml
 EMAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
+
 @dataclass
 class Elf:
     name: str
@@ -22,13 +23,16 @@ class Elf:
     def __repr__(self):
         return f"Elf(name={self.name}, secrets={', '.join([e.name for e in self.secrets])})"
 
+
 def load_elves() -> List[Elf]:
     with open("elves.yml") as elf_file:
         doc = yaml.load(elf_file.read(), Loader=yaml.FullLoader)
 
     elves = {}
     for elf in doc:
-        elves[elf] = Elf(name=elf, email=doc[elf].get("email"), secrets=[], significant=None)
+        elves[elf] = Elf(
+            name=elf, email=doc[elf].get("email"), secrets=[], significant=None
+        )
 
     # Assign signficant others
     for elf in doc:
@@ -64,12 +68,14 @@ def draw_round(elves: Dict[str, Elf]) -> Dict[str, Elf]:
 
             return elves
 
+
 def get_email_server(username: str, password: str) -> smtplib.SMTP_SSL:
     # Create a secure SSL context
     context = ssl.create_default_context()
     server = smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context)
     server.login(username, password)
     return server
+
 
 def send_emails(elves: Dict[str, Elf]) -> None:
     emails = []
@@ -90,6 +96,7 @@ def send_emails(elves: Dict[str, Elf]) -> None:
     with get_email_server(EMAIL_USERNAME, EMAIL_PASSWORD) as server:
         for receiver, message in emails:
             server.sendmail(EMAIL_USERNAME, [receiver], message.as_string())
+
 
 elves = load_elves()
 elves = draw_round(elves)
